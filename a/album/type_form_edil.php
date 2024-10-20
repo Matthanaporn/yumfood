eb096a72ce065c66de0f5c64be199496f3887056
Matthanaporn <65010912524@msu.ac.th>
2024-10-20 12:45:34 +0700

    edit

--- a/album/type_form_edil.php
+++ a/album/type_form_edil.php
@@ -247,7 +247,7 @@ include('connectdb.php');  //ไฟล์เชื่อมต่อกับ da
 //ทำการปรับปรุงข้อมูลที่จะแก้ไขลงใน database 
   
   $sql = "UPDATE category SET  
-      c_came='$c_name' 
+      c_name='$c_name' 
       WHERE c_id='$c_id' ";
 
 $result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error());
